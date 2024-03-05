7d13e7700befd78df18ac9a75c08756d12514ddb
yussycodes <157826485+yussycodes@users.noreply.github.com>
2024-03-05 13:00:53 +0100

    Update files

---
+++ script.js
@@ -0,0 +1,24 @@
+let mobileMenuList = document.getElementById("menuLink");
+let openMobileMenuButton = document.getElementById("openMobileMenu");
+let closeMobileButton = document.getElementById("closeMobileMenu");
+
+openMobileMenuButton.addEventListener('click', function(){
+     openMobileMenuButton.style.display = "none"
+     mobileMenuList.style.display = "flex"
+     closeMobileButton.style.display = "block"
+ })
+
+closeMobileButton.addEventListener('click',function(){
+    openMobileMenuButton.style.display = "block"
+     mobileMenuList.style.display = "none"
+     closeMobileButton.style.display = "none"
+    });
+
+    function showTab(tabName){
+        const tabs = document.querySelectorAll('.tab-content')
+        tabs.forEach(tab=>{
+            tab.classList.remove('active-tab')
+        })
+        const selectedTabs = document.getElementById(tabName)
+        selectedTabs.classList.add('active-tab')
+    }
